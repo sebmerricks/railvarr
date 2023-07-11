dbeecc044ef22b273b0deef7eeaf87a53fd29d2e
Sebastian Merricks <b8072600@newcastle.ac.uk>
2023-07-11 17:10:17 +0100

    Add read_centrix() function and associated documentation

--- R/wrangle_centrix.R
+++ R/wrangle_centrix.R
@@ -1,20 +1,32 @@
-env <- new.env(parent = emptyenv())
-env$raw_centrix <- NA
-
-# 2 approaches possible here:
-#    1. act as a helper function for wrangle_centrix()
-#    2. act as a separate function that reads the raw data into a stream / saves
-#       it locally so that wrangle_centrix() can use it when needed
-# I think I prefer option 2
-
+#' Read Centrix Data From Multiple Files
+#'
+#' @param path A string pointing to the directory pointing to the relevant files.
+#' @param names A character vector containing the column names.
+#' @param types A column specification created with \code{\link[readr]{cols}}.
+#'
 #' @export
-read_centrix <- function(path) {
-  env$raw_centrix <- path
+read_centrix <- function(path, names, types) {
+  # find all the files inside the directory at `path`
+  filenames <- glue::glue("{path}/{list.files(path)}")
+  # throw an error if the directory is empty
+  stopifnot("`path` must not be an empty directory" = length(filenames) > 0)
+
+  raw_centrix <- purrr::imap_dfr(filenames, ~ {
+    readr::read_csv(
+      file = .x,
+      col_names = names,
+      col_types = types,
+      skip = 1L
+    ) %>%
+      dplyr::mutate(batch = .y)
+  })
+
+  raw_centrix
 }
 
 # takes the raw data as input and returns the fully processed data
 
 #' @export
 wrangle_centrix <- function() {
-  env$raw_centrix
+
 }
