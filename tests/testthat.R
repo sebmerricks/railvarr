88b4f9828ed8b04be3486d8fee765544b50bd3e6
Sebastian Merricks <b8072600@newcastle.ac.uk>
2023-07-11 19:17:04 +0100

    Rename to railvarr

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/reference/test_package.html#special-files
 
 library(testthat)
-library(rapidrail)
+library(railvarr)
 
-test_check("rapidrail")
+test_check("railvarr")
