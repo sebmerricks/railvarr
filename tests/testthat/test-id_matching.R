7b8d20f92076bb7ee6df79fed869c6dbeb76e7ab
Sebastian Merricks <b8072600@newcastle.ac.uk>
2023-07-18 16:45:41 +0100

    Add test-id_matching.R

---
+++ tests/testthat/test-id_matching.R
@@ -0,0 +1,10 @@
+# Step 1: read in train classes data
+# Step 2: read in berth events data
+# Step 3: preprocess berth data using preprocess_berths()
+# Step 4: read in timetable calling patterns data
+# Step 5: read in timetable data
+# Step 6: preprocess timetable data using preprocess_timetable()
+# Step 7: ensure that network map has been set with geos
+# Step 8: create the match maps for each group
+# Step 9: match the ids with match_ids()
+# (Optional): check matches and accuracy using combine_ids() and accuracy()
