0a31ddb91d5b7f14cac7ddecbad1a8745b1453ae
Sebastian Merricks <b8072600@newcastle.ac.uk>
2023-08-08 11:56:27 +0100

    Add new timetable filtering algorithm

--- R/timetable.R
+++ R/timetable.R
@@ -1,26 +1,51 @@
-wrangle_timetable <- function(timetable,
-                              stations,
-                              event_map = event_mapping) {
-  # validate timetable
-  # validate stations
-  # validate event mapping
-
-  timetable <- timetable %>%
-    inner_join(event_map) %>%
-    select(-"event") %>%
-    rename(event = name)
-
-  start <- first(stations)
-  end <- last(stations)
-  forwards <- paste0(start[1], "-", end[1])
-  backwards <- paste0(end[1], "-", start[1])
+wrangle_timetable <- function(timetable, stations) {
+  validate_timetable(timetable)
+  validate_stations(stations)
 
   timetable_subset <- timetable %>%
-    find_relevant_services() %>%
-    filter_forward_services() %>%
-    remove_edge_cases() %>%
-    subset_timetable() %>%
+    filter_relevant_services(stations) %>%
+    filter_relevant_direction(stations) %>%
     find_calling_patterns()
 
   return(timetable_subset)
+}
+
+filter_relevant_services <- function(timetable, stations) {
+  return(timetable %>%
+           filter(geo %in% unlist(stations)) %>%
+           mutate(
+             across(c(dt_origin, wtt, t),
+                    ~lubridate::with_tz(.x, tzone = "Europe/London"))
+           ))
+}
+
+filter_relevant_direction <- function(timetable, stations) {
+  return(timetable %>%
+    filter(.data$geo %in% unlist(stations)) %>%
+    mutate(is_first = .data$geo %in% first(stations),
+           is_last = .data$geo %in% last(stations)) %>%
+    group_by(.data$train_header, .data$dt_origin) %>%
+    filter(first(.data$is_first) & last(.data$is_last)) %>%
+    select(-"is_first", -"is_last"))
+}
+
+
+find_calling_patterns <- function(timetable) {
+  dummy_geo <- timetable %>%
+    distinct(train_header, dt_origin) %>%
+    mutate(geo = "None", event = "Arrive")
+
+  calling_patterns <- timetable %>%
+    filter(event %in% c("Originate", "Arrive")) %>%
+    distinct(train_header, dt_origin, geo) %>%
+    bind_rows(dummy_geo) %>%
+    summarise(
+      pattern = stringr::str_c(geo, collapse = ","),
+      .groups = "drop"
+    ) %>%
+    mutate(pattern = stringr::str_replace(pattern, ",None", ""))
+
+  return(timetable %>%
+           inner_join(calling_patterns, by = c("train_header", "dt_origin")) %>%
+           ungroup())
 }
---
+++ R/validate_timetable.R
@@ -0,0 +1,19 @@
+validate_timetable <- function(timetable) {
+  stop_if_not(all(c("train_header", "dt_origin", "geo", "event", "wtt", "t",
+                    "delay", "allow") %in% names(timetable)),
+              msg = "Timetable data requires the columns 'train_header', 'dt_origin', 'geo', 'event', 'wtt', 't', 'delay', and 'allow'.")
+  stop_if_not(is.character(timetable$train_header))
+  stop_if_not(lubridate::is.POSIXct(timetable$dt_origin))
+  stop_if_not(is.character(timetable$geo))
+  stop_if_not(is.character(timetable$event))
+  stop_if_not(all(timetable$event %in% c("Pass", "Arrive", "Depart", "Originate",
+                                         "Terminate")))
+  stop_if_not(lubridate::is.POSIXct(timetable$wtt))
+  stop_if_not(lubridate::is.POSIXct(timetable$t))
+  stop_if_not(is.numeric(timetable$delay))
+  stop_if_not(is.numeric(timetable$allow))
+}
+
+validate_stations <- function(stations) {
+  stop_if_not(is.list(stations))
+}
